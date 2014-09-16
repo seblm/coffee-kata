11e6937f757b276dfb9c958d9dd17f5f4b55f537
scherrda <dahlia.scherr@gmail.com>
2014-09-17 00:26:56 +0200

    clean and webRTC staff

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,31 +1,48 @@
 $(document).ready(function () {
 	//Solution 1 webRTC
-	//see http://www.html5rocks.com/en/tutorials/getusermedia/intro/	
-	video = document.getElementById("live")	
+	//see http://www.html5rocks.com/en/tutorials/getusermedia/intro/
+    var video = document.querySelector('video');
+    var canvas = document.querySelector('canvas');
+    var ctx = canvas.getContext('2d');
+    var localMediaStream = null;
+    document.querySelector('#shot').addEventListener('click', snapshot, false);    
+	
    navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
+   
+   if(navigator.getMedia){
+	    navigator.getMedia({video:true, audio:false}, onSuccess, onError);	   
+   }else{
+	   fallback();
+   }
 
-    navigator.getMedia(
-        // constraints
-        {video:true, audio:false},
-
-        // success callback
-        function (mediaStream) {
-            var video = document.getElementsByTagName('video')[0];
-            video.src = window.URL.createObjectURL(mediaStream);
-            video.play();
-        },   
-        //handle error
-        function (error) {
-            console.log(error);
-        })  	
+    
+   function fallback(e) {
+	   video.src = 'fallbackvideo.webm';
+   }
+   function onSuccess(stream) {
+	   video.src = window.URL.createObjectURL(stream);
+	   localMediaStream = stream;
+   }
+   function onError(error) {
+       console.log(error);
+   }   
+    
+    
+    function snapshot() {
+    	console.log("snapshot");
+	    if (localMediaStream) {
+	      ctx.drawImage(video, 0, 0);
+	      // "image/webp" works in Chrome.
+	      // Other browsers will fall back to image/png.
+	      $('img').src = canvas.toDataURL('image/webp');
+	    }
+	 }   
+    
 	
       //solution 2 using scriptcam.js  
     //see http://www.scriptcam.com/docs.cfm
-	$("#webcam").scriptcam({
-		path: 'vendor/'
-	}
-					
-	);
-	
+//	$("#webcam").scriptcam({
+//		path: 'vendor/'
+//	});	
 	
 });
