3301c4568f6ac57b2bdf920a2952a98e2663e2e1
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-16 23:10:09 +0200

    remove dependencies and takes snapshot

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,22 +1,32 @@
-$(document).ready(function () {
-	//http://arstechnica.com/business/2012/01/hands-on-building-an-html5-photo-booth-with-chromes-new-webcam-api/	
-	video = document.getElementById("live")
+navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
 
-    navigator.webkitGetUserMedia("video",
-        function(stream) {
-          video.src = window.webkitURL.createObjectURL(stream)
-        },
-        function(err) {
-          console.log("Unable to get video stream!")
-        }
-    )	
-	
-    http://www.scriptcam.com/docs.cfm
-	$("#webcam").scriptcam({
-		path: 'vendor/'
-	}
-					
-	);
-	
-	
-});
+var constraints = {audio: false, video: true};
+var video = document.querySelector("video");
+var canvas = document.querySelector("canvas");
+var ctx = canvas.getContext("2d");
+
+function successCallback(stream) {
+    window.stream = stream; // stream available to console
+    if (window.URL) {
+        video.src = window.URL.createObjectURL(stream);
+    } else {
+        video.src = stream;
+    }
+}
+
+function errorCallback(error) {
+    console.log("navigator.getUserMedia error: ", error);
+}
+
+function snapshot() {
+    if (window.stream) {
+        ctx.drawImage(video, 0, 0, 640, 480, 0, 0, 300, 150);
+        // "image/webp" works in Chrome.
+        // Other browsers will fall back to image/png.
+        document.querySelector('img').src = canvas.toDataURL('image/webp');
+    }
+}
+
+video.addEventListener('click', snapshot, false);
+
+navigator.getUserMedia(constraints, successCallback, errorCallback);
