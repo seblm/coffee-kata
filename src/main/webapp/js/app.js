a0809de92942b10b4befd3563d93d70d5fb9dd2e
scherrda <dahlia.scherr@gmail.com>
2014-09-16 22:41:33 +0200

    fix js

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -1,17 +1,26 @@
 $(document).ready(function () {
-	//http://arstechnica.com/business/2012/01/hands-on-building-an-html5-photo-booth-with-chromes-new-webcam-api/	
-	video = document.getElementById("live")
+	//Solution 1 webRTC
+	//see http://www.html5rocks.com/en/tutorials/getusermedia/intro/	
+	video = document.getElementById("live")	
+   navigator.getMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia);
 
-    navigator.webkitGetUserMedia("video",
-        function(stream) {
-          video.src = window.webkitURL.createObjectURL(stream)
-        },
-        function(err) {
-          console.log("Unable to get video stream!")
-        }
-    )	
+    navigator.getMedia(
+        // constraints
+        {video:true, audio:false},
+
+        // success callback
+        function (mediaStream) {
+            var video = document.getElementsByTagName('video')[0];
+            video.src = window.URL.createObjectURL(mediaStream);
+            video.play();
+        },   
+        //handle error
+        function (error) {
+            console.log(error);
+        })  	
 	
-    http://www.scriptcam.com/docs.cfm
+      //solution 2 using scriptcam.js  
+    //see http://www.scriptcam.com/docs.cfm
 	$("#webcam").scriptcam({
 		path: 'vendor/'
 	}
