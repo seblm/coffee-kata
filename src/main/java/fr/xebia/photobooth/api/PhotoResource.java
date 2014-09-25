920ef6da387c31a64ea3f6f3945d122e6cfbbe71
Sébastian Le Merdy <sebastian.lemerdy@gmail.com>
2014-09-25 20:13:43 +0200

    Add a resource to store a file with URL.

--- src/main/java/fr/xebia/photobooth/api/PhotoResource.java
+++ src/main/java/fr/xebia/photobooth/api/PhotoResource.java
@@ -3,16 +3,17 @@ package fr.xebia.photobooth.api;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.URL;
+import java.nio.file.Files;
 import java.util.Base64;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
-
 import fr.xebia.photobooth.domain.Logic;
 import fr.xebia.photobooth.domain.Order;
 
@@ -21,38 +22,47 @@ import fr.xebia.photobooth.domain.Order;
 @Produces(MediaType.APPLICATION_JSON)
 public class PhotoResource {
 
-	@GET
-	public String getHelloWorld() {
-		return "Hello world";
-	}	
-	
-	
-	@POST
-    @Path("/create")
-	public void newPhoto(Order order) {
-		Logic.INSTANCE.run(order);
-	}	
+    @GET
+    public String getHelloWorld() {
+        return "Hello world";
+    }
 
-	@POST
+
+    @POST
+    @Path("/create")
+    public void newPhoto(Order order) {
+        Logic.INSTANCE.run(order);
+    }
+
+    @POST
     @Path("/save")
-	public String saveToFile(@PathParam("picture") String base64Picture) {
-		File urlFile = null;
-		byte[] data = Base64.getDecoder().decode(base64Picture);
-		
-		try {
-			urlFile = File.createTempFile("image", ".png");
-			OutputStream stream = new FileOutputStream(urlFile);
-			stream.write(data);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}		
-		return urlFile.getName();
-	}	
-	
+    public String saveToFile(@PathParam("picture") String base64Picture) throws IOException {
+        byte[] data = Base64.getDecoder().decode(base64Picture);
+        File urlFile = File.createTempFile("image", ".png");
+
+        try (OutputStream stream = new FileOutputStream(urlFile);) {
+            stream.write(data);
+        }
+
+        return urlFile.getName();
+    }
+
+    @POST
+    @Path("/saveWithURL")
+    public String saveToFileWithURL(String pictureUrl) throws IOException {
+        File targetFile = File.createTempFile("image", ".png");
+        targetFile.delete();
+
+        try (InputStream in = new URL(pictureUrl).openStream()) {
+            Files.copy(in, targetFile.toPath());
+        }
+
+        return targetFile.getName();
+    }
+
 	
 	/*
-	 * 
+     *
 	 * // Note preferred way of declaring an array variable
 	 */
 }
